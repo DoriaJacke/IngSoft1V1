91e8ea47beb1d0d72d54c7e647dc60618796b0ba
DoriaJacke <acv1324@gmail.com>
2025-10-04 22:50:06 -0300

    Actualiza el README con instrucciones para ejecutar el proyecto

---
+++ src/types/index.ts
@@ -0,0 +1,27 @@
+export interface Event {
+  id: string;
+  title: string;
+  artist: string;
+  date: string;
+  time: string;
+  venue: string;
+  location: string;
+  price: number;
+  image: string;
+  description: string;
+  category: string;
+  availableTickets: number;
+}
+
+export interface User {
+  id: string;
+  email: string;
+  name: string;
+  lastName: string;
+}
+
+export interface TicketSelection {
+  eventId: string;
+  quantity: number;
+  totalPrice: number;
+}
